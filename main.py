fc10a5142df2e7c2dc9f2cf7a89ee8fd5bc36b0c
geanyhalav <dumitrachegeanigabriel@gmail.com>
2023-06-27 14:43:19 +0300

    add vpn change

--- main.py
+++ main.py
@@ -10,42 +10,46 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from windscribe import windscribe
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 import const
 
 
 def write_to_csv(companies, filename, bilant_filename):
-    with open(filename, 'w', newline='') as file:
+    with open(filename, 'a+', newline='') as file:
         writer = csv.writer(file)
         # Writing headers (field names)
         writer.writerow(
-            ["Name", "CUI", "Registration Number", "EUID", "Start Date", "Description", "State", "City", "Address", "Phones", "Fax", "Email",
+            ["Name", "CUI", "Registration Number", "EUID", "Start Date", "Description", "State", "City", "Address",
+             "Phones", "Fax", "Email",
              "Website", "CAEN", "Activity", "Description of Activity"])
         # Writing data rows
         for company in companies:
             writer.writerow(
-                [company.name, company.cui, company.nr_inmatriculare, company.euid, company.start_date, company.company_description, company.state,
-                 company.city, company.address, company.phones, company.fax, company.email, company.website, company.caen, company.activity,
+                [company.name, company.cui, company.nr_inmatriculare, company.euid, company.start_date,
+                 company.company_description, company.state,
+                 company.city, company.address, company.phones, company.fax, company.email, company.website,
+                 company.caen, company.activity,
                  company.description_of_activity])
 
-    with open(bilant_filename, 'w', newline='') as bilant_file:
+        file.flush()
+    with open(bilant_filename, 'a+', newline='') as bilant_file:
         bilant_writer = csv.writer(bilant_file)
         # Writing headers (field names)
         bilant_writer.writerow(["Company Name", "Bilant"])
         # Writing data rows
         for company in companies:
             bilant_writer.writerow([company.name, company.bilant])
-
-
-# Function to change the IP address
-def change_ip():
-    pass
-    # Your code to change the IP
+            bilant_file.flush()
 
 
 # Function to return a random sleep time
 def random_sleep():
-    return random.randint(3, 10)
+    windscribe.connect(rand=True)
+    return random.randint(1, 3)
 
 
 # The login function
@@ -55,19 +59,23 @@ def login(driver, username, password):
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rememlg"]'))).click()
 
     username_field = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[1]')))
+        EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[1]')))
     username_field.send_keys(username)
 
     password_field = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[2]')))
+        EC.presence_of_element_located(
+            (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[2]')))
     password_field.send_keys(password)
 
     WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[3]'))).click()
+        EC.element_to_be_clickable(
+            (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[3]'))).click()
 
 
 class Company:
-    def __init__(self, name, cui, nr_inmatriculare, euid, start_date, company_description, state, city, address, phones, fax, email, website, caen, activity, description_of_activity, bilant):
+    def __init__(self, name, cui, nr_inmatriculare, euid, start_date, company_description, state, city, address, phones,
+                 fax, email, website, caen, activity, description_of_activity, bilant):
         self.name = name
         self.cui = cui
         self.nr_inmatriculare = nr_inmatriculare
@@ -89,85 +97,6 @@ class Company:
     def __str__(self):
         return f"Company Name: {self.name}, CUI: {self.cui}, Registration Number: {self.nr_inmatriculare}, EUID: {self.euid}, Start Date: {self.start_date}, Description: {self.company_description}, State: {self.state}, City: {self.city}, Address: {self.address}, Phones: {self.phones}, Fax: {self.fax}, Email: {self.email}, Website: {self.website}, CAEN: {self.caen}, Activity: {self.activity}, Description of Activity: {self.description_of_activity}, Bilant: {self.bilant}"
 
-def process_data1(firm_link):
-    with requests.Session() as s:
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'}
-        response = s.get(firm_link, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Initialize all fields to an empty string
-    name = cui = nr_inmatriculare = euid = start_date = company_description = state = city = address = phones = fax = email = website = caen = activity = description_of_activity = bilant = ""
-
-    info_table = soup.find(id="date-de-identificare")
-    trs = info_table.find_all('tr')
-    for i in range(len(trs)):
-        if 'Nume firmă' in trs[i].text:
-            name = trs[i].text.split('\r\n')[2].strip()
-        if 'Cod Unic de Înregistrare' in trs[i].text:
-            cui = trs[i].text.split('\r\n')[2].strip()
-        if 'Nr. Înmatriculare' in trs[i].text:
-            nr_inmatriculare = trs[i].text.split('\r\n')[2].strip()
-        if 'EUID' in trs[i].text:
-            euid = trs[i].text.split('\r\n')[2].strip()
-        if 'Data înfiinţării' in trs[i].text:
-            start_date = trs[i].text.split('\r\n')[2].strip()
-
-    # print(soup.find(id='marci-inregistrate').text) useless
-    try:
-        company_description = soup.find(id='descriere-firma').text.strip()
-    except:
-        company_description = ""
-
-    contact_table = soup.find(id="contact")
-    trs = contact_table.find_all('tr')
-    for i in range(len(trs)):
-        row = trs[i]
-        if 'Judeţ' in row.text:
-            start = len('Judeţ')
-            state = row.text[start:].strip()
-        if 'Localitate/Sector' in row.text:
-            start = len('Localitate/Sector')
-            city = row.text[start:].strip()
-        if 'Adresă' in row.text:
-            start = len('Adresă')
-            address = row.text[start:].strip()
-        if 'Mobil' in row.text:
-            start = len('Mobil')
-            phones = row.text[start:].strip()
-        if 'Fax' in row.text:
-            start = len('Fax')
-            fax = row.text[start:].strip()
-        if 'Email' in row.text:
-            start = len('Email')
-            email = row.text[start:].strip()
-        if 'Adresă web' in row.text:
-            start = len('Adresă web   ')
-            website = row.text[start:].strip()
-
-    activity_table = soup.find(id="domeniu-de-activitate")
-    trs = activity_table.find_all('tr')
-    for i in range(len(trs)):
-        row = trs[i]
-        if 'Cod CAEN' in row.text:
-            start = len('Cod CAEN')
-            caen = row.text[start:].strip()
-        if 'Obiect Activitate' in row.text:
-            start = len('Obiect Activitate')
-            activity = row.text[start:].strip()
-        if 'Descriere Activitate' in row.text:
-            start = len('Descriere Activitate')
-            description_of_activity = row.text[start:].strip()
-    bilant = soup.find(id='bilant')
-    try:
-        bilant = bilant.text
-    except:
-        bilant = ""
-
-
-    company = Company(name, cui, nr_inmatriculare, euid, start_date, company_description, state, city, address, phones, fax, email, website, caen, activity, description_of_activity, bilant)
-
-    return company
-
 
 def process_data(firm_link, driver: WebDriver):
     random_sleep()
@@ -176,7 +105,6 @@ def process_data(firm_link, driver: WebDriver):
     driver.execute_script(f"window.scrollTo(0, {randam})")
     # Initialize all fields to an empty string
     name = cui = nr_inmatriculare = euid = start_date = company_description = state = city = address = phones = fax = email = website = caen = activity = description_of_activity = bilant = ""
-
 
     info_table = driver.find_element(By.ID, "date-de-identificare")
     trs = info_table.find_elements(By.TAG_NAME, 'tr')
@@ -249,8 +177,8 @@ def process_data(firm_link, driver: WebDriver):
     except:
         bilant = ""
 
-
-    company = Company(name, cui, nr_inmatriculare, euid, start_date, company_description, state, city, address, phones, fax, email, website, caen, activity, description_of_activity, bilant)
+    company = Company(name, cui, nr_inmatriculare, euid, start_date, company_description, state, city, address, phones,
+                      fax, email, website, caen, activity, description_of_activity, bilant)
 
     return company
 
@@ -274,16 +202,18 @@ def parse_firms_page(driver: WebDriver):
 
 # Main flow
 if __name__ == "__main__":
+    windscribe.login('geanyhalav', 'croco2001')
+
     username = const.email
     password = const.password
 
     ua = UserAgent()
     user_agent = ua.random
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument(f'user-agent={user_agent}')
-
-    driver = webdriver.Chrome(chrome_options)
+    options = Options()
+    options.add_argument("start-maximized")
+    options.add_argument(f'user-agent={user_agent}')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
     try:
         login(driver, username, password)
@@ -301,4 +231,3 @@ if __name__ == "__main__":
 
     finally:
         driver.quit()
-
