5ab09052571e2af76acf0324abfd37fba9783059
geanyhalav <dumitrachegeanigabriel@gmail.com>
2023-07-20 15:11:01 +0300

    add vpn change

--- const.py
+++ const.py
@@ -1,3 +1,78 @@
+import csv
+
+from selenium.webdriver.common.by import By
+
 email='Florinciobanu91@yahoo.com'
 password='16Florin!'
 
+
+def write_header():
+    with open("companies.csv", 'a+', newline='') as file:
+        writer = csv.writer(file)
+        # Writing headers (field names)
+        writer.writerow(
+            ["Name", "CUI", "Registration Number", "EUID", "Start Date", "Description", "State", "City", "Address",
+             "Phones", "Fax", "Email",
+             "Website", "CAEN", "Activity", "Description of Activity"])
+
+
+class Company:
+    def __init__(self, name, cui, nr_inmatriculare, euid, start_date, company_description, state, city, address, phones,
+                 fax, email, website, caen, activity, description_of_activity, bilant):
+        self.name = name
+        self.cui = cui
+        self.nr_inmatriculare = nr_inmatriculare
+        self.euid = euid
+        self.start_date = start_date
+        self.company_description = company_description
+        self.state = state
+        self.city = city
+        self.address = address
+        self.phones = phones
+        self.fax = fax
+        self.email = email
+        self.website = website
+        self.caen = caen
+        self.activity = activity
+        self.description_of_activity = description_of_activity
+        self.bilant = bilant
+
+    def __str__(self):
+        return f"Company Name: {self.name}, CUI: {self.cui}, Registration Number: {self.nr_inmatriculare}, EUID: {self.euid}, Start Date: {self.start_date}, Description: {self.company_description}, State: {self.state}, City: {self.city}, Address: {self.address}, Phones: {self.phones}, Fax: {self.fax}, Email: {self.email}, Website: {self.website}, CAEN: {self.caen}, Activity: {self.activity}, Description of Activity: {self.description_of_activity}, Bilant: {self.bilant}"
+
+
+def write_to_csv(companies, filename, bilant_filename):
+    with open(filename, 'a+', newline='') as file:
+        writer = csv.writer(file)
+        # Writing data rows
+        for company in companies:
+            writer.writerow(
+                [company.name, company.cui, company.nr_inmatriculare, company.euid, company.start_date,
+                 company.company_description, company.state,
+                 company.city, company.address, company.phones, company.fax, company.email, company.website,
+                 company.caen, company.activity,
+                 company.description_of_activity])
+        file.flush()
+    with open(bilant_filename, 'a+', newline='') as bilant_file:
+        bilant_writer = csv.writer(bilant_file)
+        # Writing headers (field names)
+        bilant_writer.writerow(["Company Name", "Bilant"])
+        # Writing data rows
+        for company in companies:
+            bilant_writer.writerow([company.name, company.bilant])
+
+
+def selectCompanyLink(row):
+    exception_occurred = False
+    try:
+        a_tags = row.find_elements(By.TAG_NAME, 'a')
+        a_tag = a_tags[1]  # prima e ceva nota
+        if '/nota.asp' in a_tag.text:  # daca e anomalie schimbam
+            print("anomaliie!")
+            a_tag = a_tags[0]
+
+        firm_link = a_tag.get_property('href')
+    except:
+        exception_occurred = True
+        firm_link = None
+    return exception_occurred, firm_link
--- main.py
+++ main.py
@@ -1,104 +1,57 @@
-import csv
 import random
 import time
 
-import requests
-from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-from selenium import webdriver
+import undetected_chromedriver as uc
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-# from windscribe import windscribe
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import undetected_chromedriver as uc
+from windscribe import windscribe
 
 import const
+from const import write_header, Company, write_to_csv, selectCompanyLink
 
-
-def write_to_csv(companies, filename, bilant_filename):
-    with open(filename, 'a+', newline='') as file:
-        writer = csv.writer(file)
-        # Writing data rows
-        for company in companies:
-            writer.writerow(
-                [company.name, company.cui, company.nr_inmatriculare, company.euid, company.start_date,
-                 company.company_description, company.state,
-                 company.city, company.address, company.phones, company.fax, company.email, company.website,
-                 company.caen, company.activity,
-                 company.description_of_activity])
-
-        file.flush()
-    with open(bilant_filename, 'a+', newline='') as bilant_file:
-        bilant_writer = csv.writer(bilant_file)
-        # Writing headers (field names)
-        bilant_writer.writerow(["Company Name", "Bilant"])
-        # Writing data rows
-        for company in companies:
-            bilant_writer.writerow([company.name, company.bilant])
-            bilant_file.flush()
-
-
+windscribe_locations = windscribe.locations()
 # Function to return a random sleep time
 def random_sleep():
-    # windscribe.connect(rand=True)
-    return random.randint(1, 3)
+    return random.randint(1, 2)
 
 
 # The login function
-def login(driver, username, password):
-    driver.get("https://www.listafirme.ro/")
+def login(driver):
+    username = const.email
+    password = const.password
+    try:
 
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rememlg"]'))).click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="rememlg"]'))).click()
 
-    username_field = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[1]')))
-    username_field.send_keys(username)
+        username_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[1]')))
+        username_field.send_keys(username)
 
-    password_field = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[2]')))
-    password_field.send_keys(password)
+        password_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[2]')))
+        password_field.send_keys(password)
 
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[3]'))).click()
-
-
-class Company:
-    def __init__(self, name, cui, nr_inmatriculare, euid, start_date, company_description, state, city, address, phones,
-                 fax, email, website, caen, activity, description_of_activity, bilant):
-        self.name = name
-        self.cui = cui
-        self.nr_inmatriculare = nr_inmatriculare
-        self.euid = euid
-        self.start_date = start_date
-        self.company_description = company_description
-        self.state = state
-        self.city = city
-        self.address = address
-        self.phones = phones
-        self.fax = fax
-        self.email = email
-        self.website = website
-        self.caen = caen
-        self.activity = activity
-        self.description_of_activity = description_of_activity
-        self.bilant = bilant
-
-    def __str__(self):
-        return f"Company Name: {self.name}, CUI: {self.cui}, Registration Number: {self.nr_inmatriculare}, EUID: {self.euid}, Start Date: {self.start_date}, Description: {self.company_description}, State: {self.state}, City: {self.city}, Address: {self.address}, Phones: {self.phones}, Fax: {self.fax}, Email: {self.email}, Website: {self.website}, CAEN: {self.caen}, Activity: {self.activity}, Description of Activity: {self.description_of_activity}, Bilant: {self.bilant}"
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '/html/body/div[1]/header/div[1]/div/div[3]/div/ul/li/div/form/input[3]'))).click()
+        try:
+            time.sleep(3)
+            driver.switch_to.alert.accept()
+        except:
+            pass
+    except:
+        pass
 
 
-def process_data(firm_link, driver: WebDriver):
-    random_sleep()
+# Scrapes one firm page and create the Company object
+def process_data(firm_link: str, driver: WebDriver) -> Company:
     driver.get(firm_link)
-    randam = random.randint(100, 1000)
-    driver.execute_script(f"window.scrollTo(0, {randam})")
+    random_scroll(driver)
+
     # Initialize all fields to an empty string
     name = cui = nr_inmatriculare = euid = start_date = company_description = state = city = address = phones = fax = email = website = caen = activity = description_of_activity = bilant = ""
 
@@ -121,8 +74,6 @@ def process_data(firm_link, driver: WebDriver):
         if 'Data înfiinţării' in trs[i].text:
             start = len('Data înfiinţării')
             start_date = trs[i].text[start:].strip()
-
-    # print(soup.find(id='marci-inregistrate').text) useless
     try:
         company_description = driver.find_element(By.ID, 'descriere-firma').text.strip()
     except:
@@ -179,77 +130,69 @@ def process_data(firm_link, driver: WebDriver):
     return company
 
 
-def parse_firms_page(driver: WebDriver):
+def random_scroll(driver):
+    randam = random.randint(0, 1080)
+    driver.execute_script(f"window.scrollTo(0, {randam})")
+
+
+def scrape_one_page(driver: WebDriver):
     xpath = '/html/body/div[1]/main/section/div/table/tbody/tr'
     # one page of companies mining
     rows = driver.find_elements(By.XPATH, xpath)
     companies_list = []
     link_list = []
+
+    # get all links from page first
     for row in rows:
-        try:
-            a_tags = row.find_element(By.TAG_NAME, 'a')
-            firm_link = a_tags.get_property('href')
-        except:
+        exception_occurred, firm_link = selectCompanyLink(row)
+        if exception_occurred:
             continue
-        link_list.append(firm_link)
-
-    i = 0
-    for link in link_list:
-        print(f'Mining {link}')
-
+        link_list.append(firm_link
+                         )
+    # go and scrape every link
+    for j in range(len(link_list)):
+        link = link_list[j]
+        if j % 3 == 0:
+            changeVPn()
         try:
             company = process_data(link, driver)
         except Exception:
             print(f"Eroare la {link}")
-            continue
+            changeVPn()
+            j = j-1
+            continue # without incrementing J
+
         companies_list.append(company)
-        i += 1
-        print(f"A minat {i}/50 de pe aceasta pagina")
+
     write_to_csv(companies_list, 'companies.csv', 'bilant.csv')
 
 
-def write_header():
-    with open("companies.csv", 'a+', newline='') as file:
-        writer = csv.writer(file)
-        # Writing headers (field names)
-        writer.writerow(
-            ["Name", "CUI", "Registration Number", "EUID", "Start Date", "Description", "State", "City", "Address",
-             "Phones", "Fax", "Email",
-             "Website", "CAEN", "Activity", "Description of Activity"])
 
+def changeVPn():
+    global last_location_index
+    last_location_index += 1
+    if last_location_index < len(windscribe_locations):
+        windscribe.connect(windscribe_locations[last_location_index])
+    else:
+        last_location_index = 0
+        windscribe.connect(windscribe_locations[last_location_index])
 
 # Main flow
 if __name__ == "__main__":
-    # windscribe.login('geanyhalav', 'croco2001')
-
-    username = const.email
-    password = const.password
-
-    ua = UserAgent()
-    user_agent = ua.random
-
-    options = Options()
-    options.add_argument("start-maximized")
-    options.add_argument(f'user-agent={user_agent}')
+    windscribe.login('geanyhalav', 'croco2001')
 
     driver = uc.Chrome(headless=False, use_subprocess=False)
-
-    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
+    global last_location_index
+    last_location_index = 0
     try:
-        login(driver, username, password)
-        try:
-            driver.switch_to.alert.accept()
-        except:
-            pass
         write_header()
-        for i in range(1, 28157):
-            # Implement the logic to go to next page
+        for i in range(14, 28157):
+            # Go to page, login, get all data of 50 firms, switch IP
             link = f'https://www.listafirme.ro/pagini/p{i}.html'
             print(f"Scrapping page {i}")
             driver.get(link)
-            parse_firms_page(driver)
-            time.sleep(random_sleep())
-
+            login(driver)
+            scrape_one_page(driver)
+            windscribe.connect(rand=True)
     finally:
         driver.quit()
